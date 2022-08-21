dccc5e6ade88c83e74cbf7c92130cdfe77e965bd
daviszung <95515224+daviszung@users.noreply.github.com>
2022-08-21 18:42:36 -0400

    Made actions able to be stopped

--- src/client/components/skills/Woodcutting.jsx
+++ src/client/components/skills/Woodcutting.jsx
@@ -12,13 +12,17 @@ export function Woodcutting(props) {
         <div className='expBar'><div style={{background: '#5cace5', borderRadius: '25px', height: '100%', "maxWidth": '100%', width: `${props.state.userObj ? ((props.state.userObj.levels.woodcutting.exp - expTable[props.state.userObj.levels.woodcutting.level]) / (expTable[props.state.userObj.levels.woodcutting.level + 1] - expTable[props.state.userObj.levels.woodcutting.level])) * 100 : 0}%`}}></div></div>
       </div>
       <button id='normalLogBtn' className='actionBtn' onClick={() => {
-        props.setSelectedAction('Normal Log')}}>
+        if (props.selectedAction === 'Normal Log') {
+          props.setSelectedAction(null)
+        } else {props.setSelectedAction('Normal Log')}}}>
         <div>Normal Tree</div>
         <div>10xp / 3 seconds</div>
         <img className='treeImages' src='https://cdn.melvor.net/core/v018/assets/media/skills/woodcutting/normal_tree.svg'></img>
       </button>
       <button id='oakLogBtn' className='actionBtn' style={{display: props.state.userObj.levels.woodcutting.level >= 10 ? 'flex' : 'none'}} onClick={() => {
-        props.setSelectedAction('Oak Log')}}>
+        if (props.selectedAction === 'Oak Log') {
+          props.setSelectedAction(null)
+        } else {props.setSelectedAction('Oak Log')}}}>
         <div>Oak Tree</div>
         <div>15xp / 4 seconds</div>
         <img className='treeImages' src='https://cdn.melvor.net/core/v018/assets/media/skills/woodcutting/oak_tree.svg'></img>
