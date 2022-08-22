000a1ba63651a6352401a07448ec0dd4875936ed
daviszung <95515224+daviszung@users.noreply.github.com>
2022-08-22 11:51:40 -0400

    cleanup

--- src/client/components/skills/Woodcutting.jsx
+++ src/client/components/skills/Woodcutting.jsx
@@ -6,12 +6,11 @@ export function Woodcutting(props) {
     <div className='woodcuttingSkillContainer'>
       <div className='skillInfoContainer'>
         <div className='innerInfoContainer'>
-          <span>SKILL XP <span className='expColor'>{props.state.userObj ? props.state.userObj.levels.woodcutting.exp.toLocaleString() : null} / {props.state.userObj ? expTable[props.state.userObj.levels.woodcutting.level + 1].toLocaleString() : null}</span></span>    
-          <div>CURRENT AXE </div>
+          <span>SKILL XP <span className='expColor'>{props.state.userObj ? props.state.userObj.levels.woodcutting.exp.toLocaleString() : null} / {props.state.userObj ? expTable[props.state.userObj.levels.woodcutting.level + 1].toLocaleString() : null}</span></span>
+          <div>CURRENT AXE</div>
         </div>
         <div className='expBar'><div style={{background: '#5cace5', borderRadius: '25px', height: '100%', "maxWidth": '100%', width: `${props.state.userObj ? ((props.state.userObj.levels.woodcutting.exp - expTable[props.state.userObj.levels.woodcutting.level]) / (expTable[props.state.userObj.levels.woodcutting.level + 1] - expTable[props.state.userObj.levels.woodcutting.level])) * 100 : 0}%`}}></div></div>
       </div>
-      
       <button id='normalLogBtn' className='actionBtn' onClick={() => {
         if (props.selectedAction === 'Normal Log') {
           props.setSelectedAction(null)
