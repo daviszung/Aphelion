619467817706021a3088afb55fc4c69c0604af07
daviszung <95515224+daviszung@users.noreply.github.com>
2022-08-23 09:46:57 -0400

    some bank prototyping

--- src/client/components/economy/Bank.jsx
+++ src/client/components/economy/Bank.jsx
@@ -1,13 +1,42 @@
+import { useEffect } from 'react'
+
+
+
 export function Bank(props) {
+  const myArr = [];
+
+  function loadItems () {
+    const bank = props.state.userObj.bank;
+    
+    const storage = document.querySelector('#storage');
+    
+    for (let i in bank) {
+      myArr.push(<div key={i}>{i} : {bank[i]}</div>)
+    }
+    return myArr;
+  }
+
+  useEffect(() => {
+    const bank = props.state.userObj.bank;
+    const storage = document.querySelector('#storage');
+    
+    for (let i in bank) {
+      myArr.push(<div>{i} : {bank[i]}</div>)
+    }
+  }, [])
+
   return (
     <div id="bankContainer">
-      <div id="bankMain">
+      <div id="bankMain" className="bankArea">
         <div id="bankTabContainer"></div>
         <div id="bankInfo"></div>
-        <div id="storage"></div>
+        <div id="storage">{loadItems()}</div>
       </div>
-      <div id="bankSecondary">
+      <div id="bankSecondary" className="bankArea">
         <div></div>
+
+      </div>
+      <div id="keyItemsContainer" className="bankArea">
 
       </div>
     </div> 
