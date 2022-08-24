c02818342494d8a067209bfb16059a17b5c16710
daviszung <95515224+daviszung@users.noreply.github.com>
2022-08-23 22:45:38 -0400

    Added money tracking

--- src/client/components/economy/BankInfo.jsx
+++ src/client/components/economy/BankInfo.jsx
@@ -1,5 +1,25 @@
+import { itemGoldValues } from '../../tables.jsx'
+
+function calculateBankGold(bank) {
+  let total = 0;
+  if (!bank) return total;
+  for (let i in bank) {
+    total += itemGoldValues[i] * bank[i]
+  }
+  if (total > 99999) {
+    total = total.toLocaleString()
+    return total.slice(0, total.length - 3) + 'K'
+  }
+  return total.toLocaleString();
+}
+
+
 export function BankInfo(props) {
+
   return (
-    <div id="bankInfo">Space: {props.state.userObj.bankSpace}/{props.state.userObj.maxBankSpace}</div>
+    <div id="bankInfo">
+      <div className='bankInfoFlex'>Space: {props.state.userObj.bankSpace}/{props.state.userObj.maxBankSpace}</div>
+      <div className='bankInfoFlex'><img style={{height: '15px', width: '15px', margin: '0 10px 0 0'}} src='https://cdn.melvor.net/core/v018/assets/media/main/coins.svg'></img><div>{calculateBankGold(props.state.userObj.bank)}</div></div>
+    </div>
   )
 }
--- src/client/images.jsx
+++ src/client/images.jsx
@@ -3,4 +3,9 @@ export const itemImages = {
   'Oak Log': 'https://cdn.melvor.net/core/v018/assets/media/bank/logs_oak.png?2',
   'Willow Log': 'https://cdn.melvor.net/core/v018/assets/media/bank/logs_willow.png?2',
   'Teak Log': 'https://cdn.melvor.net/core/v018/assets/media/bank/logs_teak.png?2',
+  'Maple Log': 'https://cdn.melvor.net/core/v018/assets/media/bank/logs_maple.png?2',
+  'Mahogany Log': 'https://cdn.melvor.net/core/v018/assets/media/bank/logs_mahogany.png?2',
+  'Yew Log': 'https://cdn.melvor.net/core/v018/assets/media/bank/logs_yew.png?2',
+  'Magic Log': 'https://cdn.melvor.net/core/v018/assets/media/bank/logs_magic.png?2',
+  'Redwood Log': 'https://cdn.melvor.net/core/v018/assets/media/bank/logs_redwood.png?2',
 }
