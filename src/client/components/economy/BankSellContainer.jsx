6d4d69e9241a9120db001ab00a52e7aeb256a936
daviszung <95515224+daviszung@users.noreply.github.com>
2022-08-25 09:12:17 -0400

    features for bankSellContainer

--- src/client/components/economy/BankSellContainer.jsx
+++ src/client/components/economy/BankSellContainer.jsx
@@ -1,8 +1,8 @@
 import { itemGoldValues } from "../../tables"
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 export function BankSellContainer(props) {
-  const [sellAmount, setSellAmount] = useState(100)
+  const [sellAmount, setSellAmount] = useState(1)
 
   function sellItem(){
     const copyObj = props.state.userObj;
@@ -26,22 +26,20 @@ export function BankSellContainer(props) {
         <div>Sells for: {itemGoldValues[props.selectedItem]} GP each</div>
       </div>
       <div className="sellItemControls">
-        <div className="sellQuantityBoxContainer">
-          <div className="sellQuantityBox">1</div>
-          <div className="sellQuantityBox">{props.quantity.toLocaleString()}</div>
-        </div>
         <div className="sellItemFlex2">
-          <input className="rangeSlider" type='range'></input>
-        </div>
-        <div className="sellItemFlex3">
-          <input></input>
-          <div>
-            <button>All but 1</button>
-            <button>All</button>
+          <input className="sellTextInput" type="number" min="0" max={props.quantity} onChange={() => {
+            let val = document.querySelector('.sellTextInput').value;
+            if (val > props.quantity) {
+              val = props.quantity
+            }
+            setSellAmount(val)}}></input>
+          <div className="sellAllBtnContainer">
+            <button className="blueBtn" onClick={() => {setSellAmount(props.quantity - 1)}}>All but 1</button>
+            <button className="blueBtn" onClick={() => {setSellAmount(props.quantity)}}>All</button>
           </div>
           <button id="sellBtn" onClick={sellItem}>Sell Item</button>
         </div>
-        <div className="sellValueContainer"><img style={{height: '15px', width: '15px', margin: '0 10px 0 0'}} src="https://cdn.melvor.net/core/v018/assets/media/main/coins.svg"></img><div>{(sellAmount * itemGoldValues[props.selectedItem]).toLocaleString()}</div></div>
+        <div className="sellValueContainer"><div>Sell Amount: {sellAmount}</div><div className="flexCenter"><img style={{height: '15px', width: '15px', margin: '0 10px 0 0'}} src="https://cdn.melvor.net/core/v018/assets/media/main/coins.svg"></img><div>{(sellAmount * itemGoldValues[props.selectedItem]).toLocaleString()}</div></div></div>
       </div>
     </div>
   )
