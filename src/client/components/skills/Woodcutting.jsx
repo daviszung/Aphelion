3dee90eb50405ada964ab8f3d26621d3a1f25db4
daviszung <95515224+daviszung@users.noreply.github.com>
2022-09-04 19:05:26 -0400

    addressed timers with decimal places

--- src/client/components/skills/Woodcutting.jsx
+++ src/client/components/skills/Woodcutting.jsx
@@ -10,6 +10,7 @@ const axeType = {
   50: 'DRAGON AXE',
 }
 
+
 export function Woodcutting(props) {
   return (
     <div className='woodcuttingSkillContainer'>
@@ -25,7 +26,7 @@ export function Woodcutting(props) {
           props.setSelectedAction(null)
         } else {props.setSelectedAction('Normal Log')}}}>
         <div>Normal Tree</div>
-        <div>10xp / {3 * ((100 - props.state.modifiers.axe) / 100)} seconds</div>
+        <div>10xp / {(3 * (100 - props.state.modifiers.axe) / 100).toFixed(2)} seconds</div>
         <img className='treeImages' src='https://cdn.melvor.net/core/v018/assets/media/skills/woodcutting/normal_tree.svg'></img>
         <div className='progressBarContainer'>
           <div className='progressBar' style={props.selectedAction === 'Normal Log' ? {'animation': `load ${3 * ((100 - props.state.modifiers.axe) / 100)}s linear infinite`} : {width: '0%'}}></div>
@@ -36,7 +37,7 @@ export function Woodcutting(props) {
           props.setSelectedAction(null)
         } else {props.setSelectedAction('Oak Log')}}}>
         <div>Oak Tree</div>
-        <div>15xp / {4 * ((100 - props.state.modifiers.axe) / 100)} seconds</div>
+        <div>15xp / {(4 * (100 - props.state.modifiers.axe) / 100).toFixed(2)} seconds</div>
         <img className='treeImages' src='https://cdn.melvor.net/core/v018/assets/media/skills/woodcutting/oak_tree.svg'></img>
         <div className='progressBarContainer'>
           <div className='progressBar' style={props.selectedAction === 'Oak Log' ? {'animation': `load ${4 * ((100 - props.state.modifiers.axe) / 100)}s linear infinite`} : {width: '0%'}}></div>
@@ -47,7 +48,7 @@ export function Woodcutting(props) {
           props.setSelectedAction(null)
         } else {props.setSelectedAction('Willow Log')}}}>
         <div>Willow Tree</div>
-        <div>22xp / {5 * ((100 - props.state.modifiers.axe) / 100)} seconds</div>
+        <div>22xp / {(5 * (100 - props.state.modifiers.axe) / 100).toFixed(2)} seconds</div>
         <img className='treeImages' src='https://cdn.melvor.net/core/v018/assets/media/skills/woodcutting/willow_tree.svg'></img>
         <div className='progressBarContainer'>
           <div className='progressBar' style={props.selectedAction === 'Willow Log' ? {'animation': `load ${5 * ((100 - props.state.modifiers.axe) / 100)}s linear infinite`} : {width: '0%'}}></div>
@@ -58,7 +59,7 @@ export function Woodcutting(props) {
           props.setSelectedAction(null)
         } else {props.setSelectedAction('Teak Log')}}}>
         <div>Teak Tree</div>
-        <div>30xp / {6 * ((100 - props.state.modifiers.axe) / 100)} seconds</div>
+        <div>30xp / {(6 * (100 - props.state.modifiers.axe) / 100).toFixed(2)} seconds</div>
         <img className='treeImages' src='https://cdn.melvor.net/core/v018/assets/media/skills/woodcutting/teak_tree.svg'></img>
         <div className='progressBarContainer'>
           <div className='progressBar' style={props.selectedAction === 'Teak Log' ? {'animation': `load ${6 * ((100 - props.state.modifiers.axe) / 100)}s linear infinite`} : {width: '0%'}}></div>
@@ -69,7 +70,7 @@ export function Woodcutting(props) {
           props.setSelectedAction(null)
         } else {props.setSelectedAction('Maple Log')}}}>
         <div>Maple Tree</div>
-        <div>40xp / {8 * ((100 - props.state.modifiers.axe) / 100)} seconds</div>
+        <div>40xp / {(8 * (100 - props.state.modifiers.axe) / 100).toFixed(2)} seconds</div>
         <img className='treeImages' src='https://cdn.melvor.net/core/v018/assets/media/skills/woodcutting/maple_tree.svg'></img>
         <div className='progressBarContainer'>
           <div className='progressBar' style={props.selectedAction === 'Maple Log' ? {'animation': `load ${8 * ((100 - props.state.modifiers.axe) / 100)}s linear infinite`} : {width: '0%'}}></div>
@@ -80,7 +81,7 @@ export function Woodcutting(props) {
           props.setSelectedAction(null)
         } else {props.setSelectedAction('Mahogany Log')}}}>
         <div>Mahogany Tree</div>
-        <div>60xp / {10 * ((100 - props.state.modifiers.axe) / 100)} seconds</div>
+        <div>60xp / {(10 * (100 - props.state.modifiers.axe) / 100).toFixed(2)} seconds</div>
         <img className='treeImages' src='https://cdn.melvor.net/core/v018/assets/media/skills/woodcutting/mahogany_tree.svg'></img>
         <div className='progressBarContainer'>
           <div className='progressBar' style={props.selectedAction === 'Mahogany Log' ? {'animation': `load ${10 * ((100 - props.state.modifiers.axe) / 100)}s linear infinite`} : {width: '0%'}}></div>
@@ -91,7 +92,7 @@ export function Woodcutting(props) {
           props.setSelectedAction(null)
         } else {props.setSelectedAction('Yew Log')}}}>
         <div>Yew Tree</div>
-        <div>80xp / {12 * ((100 - props.state.modifiers.axe) / 100)} seconds</div>
+        <div>80xp / {(12 * (100 - props.state.modifiers.axe) / 100).toFixed(2)} seconds</div>
         <img className='treeImages' src='https://cdn.melvor.net/core/v018/assets/media/skills/woodcutting/yew_tree.svg'></img>
         <div className='progressBarContainer'>
           <div className='progressBar' style={props.selectedAction === 'Yew Log' ? {'animation': `load ${12 * ((100 - props.state.modifiers.axe) / 100)}s linear infinite`} : {width: '0%'}}></div>
@@ -102,7 +103,7 @@ export function Woodcutting(props) {
           props.setSelectedAction(null)
         } else {props.setSelectedAction('Magic Log')}}}>
         <div>Magic Tree</div>
-        <div>100xp / {20 * ((100 - props.state.modifiers.axe) / 100)} seconds</div>
+        <div>100xp / {(20 * (100 - props.state.modifiers.axe) / 100).toFixed(2)} seconds</div>
         <img className='treeImages' src='https://cdn.melvor.net/core/v018/assets/media/skills/woodcutting/magic_tree.svg'></img>
         <div className='progressBarContainer'>
           <div className='progressBar' style={props.selectedAction === 'Magic Log' ? {'animation': `load ${20 * ((100 - props.state.modifiers.axe) / 100)}s linear infinite`} : {width: '0%'}}></div>
@@ -113,7 +114,7 @@ export function Woodcutting(props) {
           props.setSelectedAction(null)
         } else {props.setSelectedAction('Redwood Log')}}}>
         <div>Redwood Tree</div>
-        <div>180xp / {15 * ((100 - props.state.modifiers.axe) / 100)} seconds</div>
+        <div>180xp / {(15 * (100 - props.state.modifiers.axe) / 100).toFixed(2)} seconds</div>
         <img className='treeImages' src='https://cdn.melvor.net/core/v018/assets/media/skills/woodcutting/redwood_tree.svg'></img>
         <div className='progressBarContainer'>
           <div className='progressBar' style={props.selectedAction === 'Redwood Log' ? {'animation': `load ${15 * ((100 - props.state.modifiers.axe) / 100)}s linear infinite`} : {width: '0%'}}></div>
