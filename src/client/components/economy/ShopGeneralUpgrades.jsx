8fe4555860a3f8598e23e6afcff25f9d441f8982
daviszung <95515224+daviszung@users.noreply.github.com>
2022-09-05 23:20:24 -0400

    modal component into shop general

--- src/client/components/economy/ShopGeneralUpgrades.jsx
+++ src/client/components/economy/ShopGeneralUpgrades.jsx
@@ -1,34 +1,19 @@
 import { bankSlotCosts } from "../../tables"
 import { useSelector, useDispatch } from 'react-redux'
-import { selectUserObject, buyExtraBankSlot } from '../../redux/userSlice'
+import { selectUserObject } from '../../redux/userSlice'
 import { useState } from "react"
+import { Modal } from "./Modal"
 
 export function ShopGeneralUpgrades() {
   const state = useSelector(selectUserObject)
   const dispatch = useDispatch()
-  const [selectedModal, setSelectedModal] = useState(null)
+  const [selectedModal, setSelectedModal] = useState('extraBankSlot')
+  const [modalCost, setModalCost] = useState(10000000)
 
 
   return (
     <div className="hw100">
-      <div className="modal">
-        <div className="modal-content">
-          <img className="modalImg" src="https://cdn.melvor.net/core/v018/assets/media/main/bank_header.svg"></img>
-          <h1>Buy 1x Extra Bank Slot?</h1>
-          <div>+1 Maximum Bank Space</div>
-          <div className="flexAlign">
-              <img style={{height: '32px', width: '32px', margin: '0 10px 0 0'}} src="https://cdn.melvor.net/core/v018/assets/media/main/coins.svg"></img>
-              <div style={{fontSize: '1.1rem'}}>{bankSlotCosts[state.maxBankSpace - 12]}</div>
-          </div>
-          <div className="flexAlign modalBtnContainer">
-            <button className="buyBtn" onClick={() => {
-              dispatch(buyExtraBankSlot())
-              document.querySelector(".modal").classList.toggle("show-modal")}
-              }>Buy</button>
-            <button className="cancelBtn" onClick={() => {document.querySelector(".modal").classList.toggle("show-modal")}}>Cancel</button>
-          </div>
-        </div>
-      </div>
+      <Modal selectedModal={selectedModal} dispatch={dispatch} cost={modalCost}/>
       <div className="shopSectionTitle">
         <img className="shopCategoryImg" src="https://cdn.melvor.net/core/v018/assets/media/main/bank_header.svg"></img>
         <div>GENERAL UPGRADES</div>
@@ -36,6 +21,8 @@ export function ShopGeneralUpgrades() {
       <div className="shopGrid">
         <button className="purchaseContainer" onClick={() => {
           if (state.gold >= bankSlotCosts[state.maxBankSpace - 12]) {
+            setSelectedModal('extraBankSlot')
+            setModalCost(bankSlotCosts[state.maxBankSpace - 12])
             document.querySelector(".modal").classList.toggle("show-modal")
           }
           }}>
