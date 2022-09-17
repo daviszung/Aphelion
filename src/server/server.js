f13da18693887c89c864a16bd7f3aad6a0db90ea
daviszung <95515224+daviszung@users.noreply.github.com>
2022-09-17 07:19:06 -0400

    build

--- src/server/server.js
+++ src/server/server.js
@@ -14,14 +14,14 @@ const PORT = 3000;
  app.use(express.json());
  app.use(express.urlencoded({ extended: true }));
 
-if (process.env.NODE_ENV && process.env.NODE_ENV.trim() === 'production') {
-  // statically serve everything in the build folder on the route '/build'
-  app.use('/build', express.static(path.join(__dirname, '../../build')));
-  // serve index.html on the route '/'
-  app.get('/', (req, res) => {
-    return res.status(200).sendFile(path.join(__dirname, '../../index.html'));
-  });
-}
+// if (process.env.NODE_ENV && process.env.NODE_ENV.trim() === 'production') {
+//   // statically serve everything in the build folder on the route '/build'
+//   app.use('/build', express.static(path.join(__dirname, '../../build')));
+//   // serve index.html on the route '/'
+//   app.get('/', (req, res) => {
+//     return res.status(200).sendFile(path.join(__dirname, '../../index.html'));
+//   });
+// }
 
 // api route handler
 app.use('/api', apiRouter);
